c142a501ac3eec5ad27753c21bd828c966b8f9ec
milocoder <milo.dewit@hotmail.com>
2022-04-26 13:38:31 +0200

    Update main.cpp

--- Nucleo/HC-SR04/main.cpp
+++ Nucleo/HC-SR04/main.cpp
@@ -2,11 +2,16 @@
 #include <cstdio>
 DigitalOut TRIG(D13);
 AnalogIn ECHO(A0);
-Timer timer;
+Timer timerMeasure;
+Ticker secTicker;
+bool performMeasurement = false;
 
-int main() {
+void tickerFunc() {
+    performMeasurement = true;
+}
+
+void measure() {
     bool timerOn = false;
-    wait_us(2500000);
 
     TRIG = 1;
     wait_us(10);
@@ -14,19 +19,31 @@ int main() {
 
     while(1) {
         if(ECHO.read() > 0.2 && !timerOn) {
-            timer.start();
+            timerMeasure.start();
             timerOn = true;
         }
         if(timerOn) {
             if(ECHO.read() < 0.2) {
-                timer.stop();
-                double pulseDuration = timer.elapsed_time().count(); // in microseconde
+                timerMeasure.stop();
+                double pulseDuration = timerMeasure.elapsed_time().count(); // in microseconde
                 double pulseDurationSeconde = pulseDuration / 1000000; // in seconde
                 double distanceCentimeter = pulseDurationSeconde * 343 * 100 / 2;
                 // tijd * snelheid * 100 (van m naar cm) / 2 (pulse gaat heen en weer, dus afstand tot object is distance / 2)
                 printf("Afstand is: %.2f cm\n", distanceCentimeter);
                 break;
             }
+        }
+    }
+    timerMeasure.reset();
+    performMeasurement = false;
+}
+
+int main() {
+    secTicker.attach(&tickerFunc, 1s);
+
+    while(1) {
+        if(performMeasurement) {
+            measure();
         }
     }
 }
